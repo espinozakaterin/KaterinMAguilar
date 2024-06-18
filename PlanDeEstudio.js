f1ce391f4356238a593f6b8091a69cc2b3a62fe2
espinozakaterin <katherineaguilar575@gmail.com>
2024-06-18 14:57:06 -0600

    repositorio kat

---
+++ PlanDeEstudio.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const asignaturas = document.querySelectorAll(".asignatura");
+    let currentOpen = [];
+    let currentIsRequired = [];
+   
+
+    asignaturas.forEach((asignatura) => {
+
+        asignatura.addEventListener("mouseenter", (e) => {
+            console.log("Entro en el div" + e.target.id);
+            e.target.classList.add("focus");
+            const requisitos = e.target.dataset.requisitos.split(",");
+            console.log(e.target.dataset);
+            if(requisitos) {
+                currentIsRequired = requisitos.map((requisito) => {
+                    return document.getElementById(requisito);
+                });
+                currentIsRequired.forEach((requisito) => {
+                    requisito.classList.add("isRequired");
+                });
+            }
+
+            const opens = e.target.dataset.opens.split(",");
+            if(opens) {
+                currentOpen = opens.map((open)=>{
+                    return document.getElementById(open);
+                });
+                currentOpen.forEach((open)=>{
+                    open.classList.add("opens");
+                });
+            }
+        });
+
+        asignatura.addEventListener("mouseleave", (e) => {
+            e.target.classList.remove("focus");
+            currentIsRequired.forEach((requisito) => {
+                requisito.classList.remove("isRequired");
+            });
+            currentIsRequired = [];
+            currentOpen.forEach((open)=>{
+                open.classList.remove("opens");
+            });
+            currentOpen = [];
+        });
+    });
+});
